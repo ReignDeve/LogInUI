ae6a00b3672c52242c7f1e855d29ffc0b418633f
bungertma89761 <bungertma89761@th-nuernberg.de>
2023-02-03 08:53:18 +0100

    Projektdateien hinzufügen.

---
+++ LogInUI/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+
+namespace LogInUI
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }        
+
+        
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+                
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();            
+        }
+
+        public void Savelogin()
+        {
+            string username = Usertxt.Text;
+            string password = Passwordtxt.Password;
+
+            string path = @"C:\Users\Public\Login.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(username + "," + password);
+                }
+                MessageBox.Show("Credentials saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string action = ((Button)sender).Content.ToString();
+
+                      
+                string username = Usertxt.Text;
+                string password = Passwordtxt.Password;
+
+                string path = @"C:\Users\Public\Login.txt";
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] values = line.Split(',');
+
+                            if (values[0] == username && values[1] == password)
+                            {
+                                MessageBox.Show("Login successful.");
+                                return;
+                            }
+                        }
+                        MessageBox.Show("Login failed.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+           
+        }
+    }
+}
